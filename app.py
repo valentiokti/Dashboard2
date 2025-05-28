321b269734bbea689c2e9b71a93ed062fedde733
Valentio <valentio@kristaltransport.com>
2025-05-28 15:48:03 +0700

    a

--- app.py
+++ app.py
@@ -164,7 +164,9 @@ if st.query_params.get("trigger") == "update":
     with st.spinner("Update Data ke Google Sheet", show_time=True):
         final_google = result[result['akun'].str.contains("Driver")]
         final_google = final_google.sort_values('date')
-        final_google['Balance'] = (final_google['Debit'] - final_google['Credit']).cumsum()
+        final_google['Balance'] = (
+        final_google['Debit'] - final_google['Credit']  # pakai 'Kredit' jika sudah rename
+    ).groupby(final_google['akun']).cumsum()
 
         final_google['Saldo'] = np.where(final_google['Debit'] == 0, final_google['Credit'] * -1, 
                                                 np.where(final_google['Credit'] == 0, final_google['Debit'], np.nan))
@@ -178,6 +180,7 @@ if st.query_params.get("trigger") == "update":
         final_google.rename(columns={'Credit': 'Kredit', 'akun':'Nama Perkiraan', 'date':'Tanggal', 'memo': 'Deskripsi'}, inplace=True)
         update_data("1cjR6k-OCWmeSfCRS_3b9B3wgQc8a-d_pIvWzcZpz1Uw","Valentio",final_google)
         st.success("Berhasil Update!!!")
+        
         
 
 
